2dc52339efbcd745705372a78e1da64d4f4691c6
Alexandra DeWit <alexa.dewit@evolvetechnology.se>
2017-10-10 12:56:17 +0200

    Light cleanup

--- src/test/scala/SnakeServiceTest.scala
+++ src/test/scala/SnakeServiceTest.scala
@@ -7,6 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import cats.effect._
 import io.circe.syntax._
 import org.http4s.dsl.io.{uri, _}
+import org.http4s.circe._
 import com.github.alexadewit.scalasnake.requests.GameStartRequest
 import com.github.alexadewit.scalasnake.requests.GameStartRequestCodec._
 import org.http4s.{Method, Request}
@@ -18,7 +19,7 @@ class SnakeServiceTest extends FlatSpec with Matchers {
   "The Root Endpoint" should "echo back the input value" in {
     val input = GameStartRequest(10, 10, UUID.randomUUID())
     val resp = for {
-      req <- Request[IO](Method.POST, uri("/start")).withBody(input.asJson.toString())
+      req <- Request[IO](Method.POST, uri("/start")).withBody(input.asJson)
       resp <- serviceFixture.route().orNotFound.run(req).as[GameStartRequest]
     } yield resp
     resp.unsafeRunSync shouldBe input
